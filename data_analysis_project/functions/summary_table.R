1e4403b26bb5c140f59779b96bfd1e5f955bb231
Spencer Pease <spencer.p.816@gmail.com>
2020-02-25 00:03:09 -0800

    [data_analysis_project] Final draft of report

--- data_analysis_project/functions/summary_table.R
+++ data_analysis_project/functions/summary_table.R
@@ -10,15 +10,16 @@ summary_table <- function(data, summary_funcs = NULL) {
   if (is.null(summary_funcs)) {
     summary_funcs <- list(
       n       = ~length(.),
-      valid   = ~sum(!is.na(.)),
+      #valid   = ~sum(!is.na(.)),
       missing = ~sum(is.na(.)),
       mean    = ~mean(., na.rm = TRUE),
       sd      = ~sd(., na.rm = TRUE),
-      min     = ~min(., na.rm = TRUE),
-      q25     = ~quantile(., 0.25, na.rm = TRUE),
+      #min     = ~min(., na.rm = TRUE),
+      #q25     = ~quantile(., 0.25, na.rm = TRUE),
       median  = ~median(., na.rm = TRUE),
-      q75     = ~quantile(., 0.75, na.rm = TRUE),
-      max     = ~max(., na.rm = TRUE)
+      #q75     = ~quantile(., 0.75, na.rm = TRUE),
+      IQR     = ~IQR(., na.rm = TRUE)
+      #max     = ~max(., na.rm = TRUE)
     )
   }
 
